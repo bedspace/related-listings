f1861c3cec6fc5456ef9ec3887965fb67bcdbf3e
Amar <amar.vadhia@gmail.com>
2019-07-19 12:23:52 -0700

    change docker-compose/dockerFiles

---
+++ server/db/Permissions.sql
@@ -0,0 +1,4 @@
+-- CREATE USER 'amar'@'localhost' IDENTIFIED BY 'password';
+-- GRANT ALL PRIVILEGES ON *.* TO 'amar'@'localhost' WITH GRANT OPTION;
+-- CREATE USER 'amar'@'%' IDENTIFIED BY 'password';
+-- GRANT ALL PRIVILEGES ON *.* TO 'amar'@'%' WITH GRANT OPTION;
